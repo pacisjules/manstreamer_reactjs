8ef56f57349307d896484dd93f319816700ccb54
Ishimwe Jules Pacis (Mediator) <51479761+pacisjules@users.noreply.github.com>
2022-07-03 23:13:16 +0200

    starting

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# reactjs_togglecss
+ This project show how styles in react js show you how toggle functions work
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+
+export default class App extends React.Component {
+  state = {
+    bgcolor: "#696969",
+    switchs: true,
+    anime:"",
+    ctcolor: "#cbd4d9",
+    ctborderrd:"20px",
+    message:"OFF",
+    photoimg: "grayscale(0%)",
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hallo me</p>
+      </div>
+    );
+  }
+}
